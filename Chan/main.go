688292bc41f2161db767570661bf2c36a9a02374
Александр Козлов <aleksandrkozlov@big.local>
2025-07-10 20:36:50 +0300

    repush Chan

--- Chan/main.go
+++ Chan/main.go
@@ -1,1 +1,19 @@
-package Chan
+package main
+
+import "fmt"
+
+func main() {
+	result := make(chan int)
+
+	array := [5]int{2, 4, 6, 8, 10}
+
+	for _, value := range array {
+		go func(value int) {
+			result <- value * value
+		}(value)
+	}
+
+	for i := 0; i < len(array); i++ {
+		fmt.Printf("%v ", <-result)
+	}
+}
